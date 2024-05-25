d8c1468a4dfcc781d4b6a4d836e5fbeb54090770
omar <omar@tzst.com>
2024-05-25 23:00:17 +0200

    Mobile

--- calcule/app/src/main/java/com/sup/calcule/MainActivity.java
+++ calcule/app/src/main/java/com/sup/calcule/MainActivity.java
@@ -3,6 +3,9 @@ package com.sup.calcule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText txt_prenom=(EditText) findViewById(R.id.txt_prenom);
+        EditText txt_nom=(EditText) findViewById(R.id.txt_nom);
+        Button btn_afficher=(Button) findViewById(R.id.btn_afficher);
+        TextView txt=(TextView) findViewById(R.id.txt_affichage);
+
+
+
+
+
     }
 }
