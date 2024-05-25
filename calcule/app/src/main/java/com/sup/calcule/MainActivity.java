5898cad6741d2d1f9865b356b01311292aa0039d
omar <omar@tzst.com>
2024-05-25 23:19:18 +0200

    Mobile

--- calcule/app/src/main/java/com/sup/calcule/MainActivity.java
+++ calcule/app/src/main/java/com/sup/calcule/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         EditText txt_prenom=(EditText) findViewById(R.id.txt_prenom);
         EditText txt_nom=(EditText) findViewById(R.id.txt_nom);
+        EditText txt_age=(EditText) findViewById(R.id.txt_age);
         Button btn_afficher=(Button) findViewById(R.id.btn_afficher);
         TextView txt=(TextView) findViewById(R.id.txt_affichage);
 
@@ -25,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String prenom=txt_prenom.getText().toString();
                 String nom=txt_nom.getText().toString();
+                Integer age= Integer.parseInt(txt_age.getText().toString());
                 txt.setText("Bonjour "+prenom +" "+nom);
+
+                Toast.makeText(MainActivity.this, "Votre Age est :"+String.valueOf(age), Toast.LENGTH_SHORT).show();
             }
         });
 
