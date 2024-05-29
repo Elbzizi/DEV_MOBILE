ba89e15326ce4a77afd1bb6bcff08b775abb0587
omar <omar@tzst.com>
2024-05-29 11:38:36 +0200

    Mobile

---
+++ Exam_Omar_El_Bzizi/app/src/main/java/com/example/myapp/model/Contact.java
@@ -0,0 +1,12 @@
+package com.example.myapp.model;
+
+public class Contact {
+
+    public String nom, tel;
+
+    public Contact(String nom, String tel) {
+        this.nom = nom;
+        this.tel = tel;
+    }
+
+}
