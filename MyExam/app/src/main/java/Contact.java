daebb90f75d4584fa225a761158db24572cc0f70
omar <omar@tzst.com>
2024-05-29 10:51:53 +0200

    Mobile

---
+++ MyExam/app/src/main/java/Contact.java
@@ -0,0 +1,17 @@
+public class Contact {
+    private String nom;
+    private String tele;
+
+    public Contact(String nom, String tele) {
+        this.nom = nom;
+        this.tele = tele;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getTele() {
+        return tele;
+    }
+}
--- MyExam/app/src/main/java/com/sup/myexam/MainActivity.java
+++ MyExam/app/src/main/java/com/sup/myexam/MainActivity.java
@@ -2,13 +2,49 @@ package com.sup.myexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+     EditText etName;
+     EditText etPhone;
+     Button btnAdd;
+     Button btnViewContacts;
+     ArrayList<Contact> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        etPhone = findViewById(R.id.etPhone);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnViewContacts = findViewById(R.id.btnViewContacts);
+        contactList = new ArrayList<Contact>();
+
+        btnAdd.setOnClickListener(v -> {
+            String name = etName.getText().toString();
+            String phone = etPhone.getText().toString();
+            if (!name.isEmpty() && !phone.isEmpty()) {
+                contactList.add(new Contact(name, phone));
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();.makeText(MainActivity.this, "Contact ajouté", Toast.LENGTH_SHORT).show();
+                etName.setText("");
+                etPhone.setText("");
+            } else {
+                Toast.makeText(MainActivity.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnViewContacts.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ContactListActivity.class);
+            intent.putParcelableArrayListExtra("contacts", contactList);
+            startActivity(intent);
+        });
     }
 }
